6445b021c7cfc50bb23bbe07160cad7969efdbc3
Jozef Nagy <schkwve@gmail.com>
2023-12-19 18:45:02 +0100

    Logger is a singleton

--- engine/logging.h
+++ engine/logging.h
@@ -12,17 +12,17 @@
 
 class Log {
 private:
-	static std::mutex lock_logging;
+	std::mutex lock_logging;
 
 	// Not yet used
-	static FILE *f_logfile;
+	FILE *f_logfile;
 
 public:
 	template<typename... Args>
 	static void TRACE(const char *fmt, Args... args)
 	{
 #ifdef __DEBUG__
-		log("TRACE", fmt, args...);
+		GetInstance().log("TRACE", fmt, args...);
 #endif
 	}
 
@@ -30,48 +30,62 @@ public:
 	static void DEBUG(const char *fmt, Args... args)
 	{
 #ifdef __DEBUG__
-		log("DEBUG", fmt, args...);
+		GetInstance().log("DEBUG", fmt, args...);
 #endif
 	}
 
 	template<typename... Args>
 	static void INFO(const char *fmt, Args... args)
 	{
-		log("INFO", fmt, args...);
+		GetInstance().log("INFO", fmt, args...);
 	}
 
 	template<typename... Args>
 	static void WARN(const char *fmt, Args... args)
 	{
-		log("WARN", fmt, args...);
+		GetInstance().log("WARN", fmt, args...);
 	}
 
 	template<typename... Args>
 	static void ERROR(const char *fmt, Args... args)
 	{
-		log("ERROR", fmt, args...);
+		GetInstance().log("ERROR", fmt, args...);
 	}
 
 	template<typename... Args>
 	static void FATAL(const char *fmt, Args... args)
 	{
-		log("FATAL", fmt, args...);
+		GetInstance().log("FATAL", fmt, args...);
 	}
 
 private:
-	template<typename... Args>
-	static void log(const char *log_level_str, const char *fmt, Args... args)
+
+	Log()
 	{
+	}
+
+	Log(const Log&) = delete;
+	Log& operator= (const Log&) = delete;
+
+	~Log()
+	{
+	}
+
+	static Log& GetInstance()
+	{
+		static Log logger;
+		return logger;
+	}
+
+	template<typename... Args>
+	void log(const char *log_level_str, const char *fmt, Args... args)
+	{
+		std::scoped_lock lock(lock_logging);
+
 		std::time_t cur_time = std::time(0);
 		std::tm *timestamp = std::localtime(&cur_time);
 		char buffer[80];
 		strftime(buffer, 80, "%c", timestamp);
-
-
-		std::scoped_lock lock(lock_logging);
 		printf("%s | [%s]: %s\n", buffer, log_level_str, fmt, args...);
 	}
 };
-
-std::mutex Log::lock_logging;
-FILE *f_logfile = NULL;
