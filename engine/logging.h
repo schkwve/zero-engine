1bc445dab8a36364ecf77592d6fac967db6acd5b
Jozef Nagy <schkwve@gmail.com>
2023-12-19 18:39:24 +0100

    Logger timestamps and small refactor

--- engine/logging.h
+++ engine/logging.h
@@ -12,15 +12,17 @@
 
 class Log {
 private:
-	static std::mutex mtx_logging;
+	static std::mutex lock_logging;
+
+	// Not yet used
+	static FILE *f_logfile;
 
 public:
 	template<typename... Args>
 	static void TRACE(const char *fmt, Args... args)
 	{
 #ifdef __DEBUG__
-		std::scoped_lock lock(mtx_logging);
-		printf("[trace]: %s\n", fmt, args...);
+		log("TRACE", fmt, args...);
 #endif
 	}
 
@@ -28,38 +30,48 @@ public:
 	static void DEBUG(const char *fmt, Args... args)
 	{
 #ifdef __DEBUG__
-		std::scoped_lock lock(mtx_logging);
-		printf("[debug]: %s\n", fmt, args...);
+		log("DEBUG", fmt, args...);
 #endif
 	}
 
 	template<typename... Args>
 	static void INFO(const char *fmt, Args... args)
 	{
-		std::scoped_lock lock(mtx_logging);
-		printf("[info]: %s\n", fmt, args...);
+		log("INFO", fmt, args...);
 	}
 
 	template<typename... Args>
 	static void WARN(const char *fmt, Args... args)
 	{
-		std::scoped_lock lock(mtx_logging);
-		printf("[warn]: %s\n", fmt, args...);
+		log("WARN", fmt, args...);
 	}
 
 	template<typename... Args>
 	static void ERROR(const char *fmt, Args... args)
 	{
-		std::scoped_lock lock(mtx_logging);
-		printf("[error]: %s\n", fmt, args...);
+		log("ERROR", fmt, args...);
 	}
 
 	template<typename... Args>
 	static void FATAL(const char *fmt, Args... args)
 	{
-		std::scoped_lock lock(mtx_logging);
-		printf("[fatal]: %s\n", fmt, args...);
+		log("FATAL", fmt, args...);
+	}
+
+private:
+	template<typename... Args>
+	static void log(const char *log_level_str, const char *fmt, Args... args)
+	{
+		std::time_t cur_time = std::time(0);
+		std::tm *timestamp = std::localtime(&cur_time);
+		char buffer[80];
+		strftime(buffer, 80, "%c", timestamp);
+
+
+		std::scoped_lock lock(lock_logging);
+		printf("%s | [%s]: %s\n", buffer, log_level_str, fmt, args...);
 	}
 };
 
-std::mutex Log::mtx_logging;
+std::mutex Log::lock_logging;
+FILE *f_logfile = NULL;
